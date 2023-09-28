3e225fffa8835ccc6689ebcc939ed03a487d36d3
moqsien <moqsien@foxmail.com>
2023-09-28 16:47:25 +0800

    bugfix for gprint

--- pkgs/gtea/gprint/print_log.go
+++ pkgs/gtea/gprint/print_log.go
@@ -20,25 +20,25 @@ const (
 )
 
 func prepareHint(hint, backgroudColor string) string {
-	return hintColor.Background(lipgloss.Color(backgroudColor)).Render(hint)
+	return hintColor.Copy().Background(lipgloss.Color(backgroudColor)).Render(hint)
 }
 
 func PrintInfo(format string, v ...any) {
-	fmt.Println(prepareHint(HintInfo, "#40E0D0") + " " + txtColor.Foreground(lipgloss.Color("#40E0D0")).Render(fmt.Sprintf(format, v...)))
+	fmt.Println(prepareHint(HintInfo, "#40E0D0") + " " + txtColor.Copy().Foreground(lipgloss.Color("#40E0D0")).Render(fmt.Sprintf(format, v...)))
 }
 
 func PrintSuccess(format string, v ...any) {
-	fmt.Println(prepareHint(HintSuccess, "#32CD32") + " " + txtColor.Foreground(lipgloss.Color("#32CD32")).Render(fmt.Sprintf(format, v...)))
+	fmt.Println(prepareHint(HintSuccess, "#32CD32") + " " + txtColor.Copy().Foreground(lipgloss.Color("#32CD32")).Render(fmt.Sprintf(format, v...)))
 }
 
 func PrintWarning(format string, v ...any) {
-	fmt.Println(prepareHint(HintWarning, "#FFFF00") + " " + txtColor.Foreground(lipgloss.Color("#FFFF00")).Render(fmt.Sprintf(format, v...)))
+	fmt.Println(prepareHint(HintWarning, "#FFFF00") + " " + txtColor.Copy().Foreground(lipgloss.Color("#FFFF00")).Render(fmt.Sprintf(format, v...)))
 }
 
 func PrintError(format string, v ...any) {
-	fmt.Println(prepareHint(HintError, "#FF6347") + " " + txtColor.Foreground(lipgloss.Color("#FF6347")).Render(fmt.Sprintf(format, v...)))
+	fmt.Println(prepareHint(HintError, "#FF6347") + " " + txtColor.Copy().Foreground(lipgloss.Color("#FF6347")).Render(fmt.Sprintf(format, v...)))
 }
 
 func PrintFatal(format string, v ...any) {
-	fmt.Println(prepareHint(HintFatal, "#FF0000") + " " + txtColor.Foreground(lipgloss.Color("#FF0000")).Render(fmt.Sprintf(format, v...)))
+	fmt.Println(prepareHint(HintFatal, "#FF0000") + " " + txtColor.Copy().Foreground(lipgloss.Color("#FF0000")).Render(fmt.Sprintf(format, v...)))
 }
