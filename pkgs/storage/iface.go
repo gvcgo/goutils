796cb03637e580f46dda0ab514f4b2712d69b190
moqsien <moqsien@foxmail.com>
2024-01-09 16:30:46 +0800

    storage interface

---
+++ pkgs/storage/iface.go
@@ -0,0 +1,10 @@
+package storage
+
+type IStorage interface {
+	CreateRepo(string) []byte
+	GetRepoInfo(string) []byte
+	GetContents(string, string, string) []byte
+	UploadFile(string, string, string, string) []byte
+	GetFileInfo(string, string, string) []byte
+	DeleteFile(string, string, string, string) []byte
+}
