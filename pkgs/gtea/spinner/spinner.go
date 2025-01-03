da05cdf70729fdb3b873b035b49a58333b54b98d
moqsien <moqsien@foxmail.com>
2025-01-04 00:03:45 +0800

    esc for spinner

--- pkgs/gtea/spinner/spinner.go
+++ pkgs/gtea/spinner/spinner.go
@@ -54,7 +54,7 @@ func (m *Spinner) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "q", "esc":
+		case "esc":
 			m.quitting = true
 			if m.sweepFunc != nil {
 				m.sweepFunc()
